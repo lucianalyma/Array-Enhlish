00ff6828a5abb638fa60ba4f1857477cb8c01b9e
lucianalyma <lu_llim@hotmail.com>
2022-07-03 10:31:58 -0300

    Array-Eng

---
+++ script.js
@@ -0,0 +1,34 @@
+let comedy = [
+    {
+        title: "White chicks",
+        director: "Keenen Ivory Wayans",
+        year: 2004,
+    },
+] ;
+
+let action = [
+    {
+        title: "Fat and furious",
+        director: "Justin Lin",
+        year: 2021,
+    },
+];
+let fantasy = [
+    {
+        title: "Lord of the rings",
+        director: "Peter Jackson",
+        year:2001,
+    },
+];
+console.log (comedy);
+console.log (action);
+console.log (fantasy);
+
+let myMovie = "The freedom writers";
+
+if (myMovie == "The freedom writers" || myMovie == "white chicks") {
+  console.log("My favorite movie is The freedom writers");
+}
+else {
+    console.log ("It isn't Whtie chicks!")
+}
